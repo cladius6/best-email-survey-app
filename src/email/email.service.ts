b76dd8f10ac9008f23e38fd596cf6fd8dc29421b
Klaudiusz <d1f1.6@protonmail.com>
2022-04-05 22:02:31 +0200

    Nodemailer with sendgrid initialized.

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import * as sendGrid from '@sendgrid/mail';
 import { ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
+import * as nodemailerSendgrid from 'nodemailer-sendgrid';
+import { EmailDTO } from './email.dto';
 
 @Injectable()
 export class EmailService {
+  private transporter;
   constructor(private configService: ConfigService) {
-    sendGrid.setApiKey(this.configService.get('SENDGRID.API_KEY'));
+    this.transporter = nodemailer.createTransport(
+      nodemailerSendgrid({
+        apiKey: this.configService.get('SENDGRID.API_KEY') as string,
+      }),
+    );
   }
 
-  async sendEmail(email): Promise<void> {
-    await sendGrid.send(email);
+  async sendEmail(email: EmailDTO): Promise<void> {
+    return await this.transporter.sendMail(email);
   }
 }
