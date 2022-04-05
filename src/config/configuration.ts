0f6fd8d9beb01f22c33034e59f2e46dba7cfeff7
Klaudiusz <d1f1.6@protonmail.com>
2022-04-05 20:04:32 +0200

    Configuration file added.

---
+++ src/config/configuration.ts
@@ -0,0 +1,5 @@
+export default () => ({
+  SENDGRID: {
+    API_KEY: process.env.SENDGRID_API_KEY,
+  },
+});
--- src/email/email.module.ts
+++ src/email/email.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EmailService } from './email.service';
 import { EmailController } from './email.controller';
+import configuration from '../config/configuration';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule.forRoot({ load: [configuration] })],
   controllers: [EmailController],
   providers: [EmailService],
 })
