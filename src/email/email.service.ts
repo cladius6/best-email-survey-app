b31165edddf587018747080f537222e039e2e4b7
Klaudiusz <d1f1.6@protonmail.com>
2022-04-05 20:29:00 +0200

    Simple sendGrid implementation in email service.

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import * as sendGrid from '@sendgrid/mail';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class EmailService {
-  sendEmail(email) {
-    console.log(`Email: ${email.to}`);
-    console.log(`Subject: ${email.subject}`);
-    console.log(`Message: ${email.message}`);
+  constructor(private configService: ConfigService) {
+    sendGrid.setApiKey(this.configService.get('SENDGRID.API_KEY'));
+    console.log(this.configService.get('SENDGRID.API_KEY'));
+  }
+
+  async sendEmail(email): Promise<boolean> {
+    try {
+      await sendGrid.send(email);
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
   }
 }
