65974e83f70ca23dbe9be785023f940f4bc6d0b4
Klaudiusz <d1f1.6@protonmail.com>
2022-04-06 14:14:06 +0200

    ConfigModule added to AppModule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmailModule } from './email/email.module';
 import { QuestionnaireModule } from './questionnaire/questionnaire.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './config/configuration';
 
 @Module({
-  imports: [EmailModule, QuestionnaireModule],
+  imports: [
+    ConfigModule.forRoot({ load: [configuration] }),
+    QuestionnaireModule,
+    EmailModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
